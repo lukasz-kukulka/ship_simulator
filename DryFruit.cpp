7b1a9571b056da7cf1b06c5e28e2e6c3c35ef47c
lukasz-kukulka <qqla83@gmail.com>
2021-03-30 06:27:52 +0100

    Add DryFruit getters and count rotten time function

--- DryFruit.cpp
+++ DryFruit.cpp
@@ -1,1 +1,15 @@
 #include "DryFruit.hpp"
+
+
+std::size_t DryFruit::getPrice() const{
+    return Fruit::getPrice() * 3;
+}
+
+void DryFruit::countRottenTime(){
+    static uint8_t dryFruitRottenTime = 0;
+    if (dryFruitRottenTime <= 10){
+        dryFruitRottenTime++;
+    } else {
+        rottenTime_--;
+    }
+} 
--- DryFruit.hpp
+++ DryFruit.hpp
@@ -6,8 +6,9 @@ class DryFruit : public Fruit{
 public:
     DryFruit();
 
+    std::size_t getPrice() const;
+    std::size_t getRottenTime() { return rottenTime_; }
 private:
     void countRottenTime();
-    
 
 };
